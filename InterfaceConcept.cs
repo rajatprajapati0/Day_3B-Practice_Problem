f5f3a62fcaaffad820dca6d5ce53341e41378d48
Rajat <rajatprajapati09@gmail.com>
2023-04-13 02:56:09 +0530

    [rajat]Interface_Concept

---
+++ InterfaceConcept.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace OppsConsepts
+{
+    internal class InterfaceConcept
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oops Consepts");
+            DemoInheritingInterFace demoInheritingInterFace = new DemoInheritingInterFace();
+            int sum = demoInheritingInterFace.sumOfTwoNum(5, 6);
+            demoInheritingInterFace.example();
+            IDemoInterFace demoObj = demoInheritingInterFace;
+            demoObj.example();
+            demoInheritingInterFace.example();
+            IDemoInterFace.StaticEx();
+            DemoInheritingInterFace.StaticEx();
+
+
+
+            Demo2InheritingInterFace demo2InheritingInterFace = new Demo2InheritingInterFace();
+            int sum2 = demo2InheritingInterFace.sumOfTwoNum(5, 6);
+            demo2InheritingInterFace.example();
+            IDemoInterFace demoObj2 = demo2InheritingInterFace;
+            demoObj2.example();
+            demo2InheritingInterFace.example();
+            IDemoInterFace.StaticEx();
+            Demo2InheritingInterFace.StaticEx();
+
+        }
+    }
+
+    interface IDemoInterFace
+    {
+        int sumOfTwoNum(int a, int b);
+
+        void example()
+        {
+            Console.WriteLine("Rajat");
+        }
+        static void StaticEx()
+        {
+            Console.WriteLine("static method");
+        }
+
+    }
+    public class DemoInheritingInterFace : IDemoInterFace
+    {
+        public int sumOfTwoNum(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public void example()
+        {
+            Console.WriteLine("Prajapati");
+        }
+
+        public static void StaticEx()
+        {
+            Console.WriteLine("static alterd method\n");
+        }
+    }
+    public class Demo2InheritingInterFace : IDemoInterFace
+    {
+        public int sumOfTwoNum(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public void example()
+        {
+            Console.WriteLine("Name");
+        }
+
+        public static void StaticEx()
+        {
+            Console.WriteLine("static alterd method");
+        }
+    }
+
+}
