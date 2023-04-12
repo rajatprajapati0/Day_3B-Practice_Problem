039621681e113c26336cfec4b937b8065f590c09
Rajat <rajatprajapati09@gmail.com>
2023-04-13 03:12:24 +0530

    [rajat]Access_Modifiers_Concepts

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OppsConsepts
+{
+    //partial is use for manege the code by spliting in two page but in same class
+    partial class Access_Modifiers_Concepts
+    {
+
+
+
+
+    }
+}
